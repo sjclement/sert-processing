dc0f77b3d77c2873f2ad4a0c999ddf15c1154242
Stephen <s.clement@techbuyer.com>
2021-08-09 16:46:25 +0100

    Better metrics and env parsing from results xml. Ouput more easily converted to Dataframe

--- parse_results.py
+++ parse_results.py
@@ -6,37 +6,45 @@ import json
 def process_results_xml(filepath):
     tree = ET.parse(filepath)
     root = tree.getroot()
-    results = parse_results(root)
+    metrics = parse_results(root)
     scores = parse_summary(root.find('run-data/summary'))
-    return merge(results, scores)
+    env = parse_test_environment(root)
+    return metrics, scores, env
 
 def parse_results(root_el):
-    results = {}
+    results = []
     for workload in root_el.findall('run-data/workload'):
         workload_type = workload.get('name')
-        results[workload_type] = parse_worklets(workload)
+        worklet_results = parse_worklets(workload)
+        for r in worklet_results:
+            r.update( {'workload': workload_type} )
+        results.extend(worklet_results)
 
     return results
 
 def parse_worklets(workload_el):
-    result = {}
+    result = []
     for worklet in workload_el.findall('worklet'):
         benchmark =  worklet.get('name')
-        result[benchmark] = parse_test(worklet)
+        result.extend(parse_test(worklet))
     return result
 
 def parse_test(worklet_el):
-    worklet_result = {}
+    worklet_result = []
     for phase in worklet_el.iter('phase'):
-
         if phase.get('type') == 'measurement':
             for test in phase.findall('sequence/interval'):
+                test_results = {}
                 interval = test.attrib['name']
-                worklet_result[interval] = parse_metrics(test.find('result'))
-                #worklet_result[interval]['score'] = parse_score(test.find('result'))
+                metrics = parse_metrics(interval, test.find('result'))
+                metrics['score'] = parse_score(test.find('result'))
+                metrics['loadlevel'] = interval
+                metrics['worklet'] = worklet_el.get('name')
+                worklet_result.append(metrics)
         elif phase.get('type') == 'calibration':
-            for calibration in phase.findall('calibrationResult'):
-                worklet_result['calibration'] = calibration.find('transactionsPerSecond').text
+            worklet_result.append({'loadlevel': 'calibration', 
+                                   'worklet': worklet_el.get('name'),
+                                   'score': phase.find('calibrationResult/transactionsPerSecond').text})
 
     return worklet_result
 
@@ -44,25 +52,21 @@ def parse_test(worklet_el):
 def parse_score(test_el):
     return test_el.find('score').text
 
-def parse_metrics(test_el):
+def parse_metrics(loadlevel, test_el):
     metrics = {}
     for m in test_el.find('metrics').iter('provider'):
         for m_type in m:
-            if m_type.tag == 'power-analyzer':
-                metrics['power'] = {}
-                for t in m_type.find('measurement').find('watts').iter():
-                    if t.tag != 'watts':
-                        metrics['power'][t.tag] = t.text
-            elif m_type.tag == 'timing':
-                metrics['timing'] = {}
-                for t in m_type.find('interval').iter():
-                        if t.tag != 'interval':
-                            metrics['timing'][t.tag] = t.text
+            if m_type.tag == 'timing':
+                metrics["start"] = m_type.find('interval/started').text
+                metrics["end"] = m_type.find('interval/ending').text
             elif m_type.tag == 'temperature-sensor':
-                metrics['temperature'] = {}
-                for t in  m_type.find('measurement').find('temperature'):
-                    if t.tag != 'temperature':
-                        metrics['temperature'][t.tag] = t.text
+                metrics["temp-min"] = m_type.find('measurement/temperature/minimum').text
+                metrics["temp-max"] = m_type.find('measurement/temperature/maximum').text
+                metrics["temp-avg"] = m_type.find('measurement/temperature/average').text
+            elif m_type.tag == 'power-analyzer':
+                metrics["watts-min"] = m_type.find('measurement/watts/minimum').text
+                metrics["watts-max"] = m_type.find('measurement/watts/maximum').text
+                metrics["watts-avg"] = m_type.find('measurement/watts/average').text
             
     return metrics
 
@@ -91,6 +95,22 @@ def parse_test_summary(worklet_el):
                 scores[t.tag] = t.text
         scores_all[load.get('name')] = scores
     return scores_all
+
+def parse_test_environment(root_el):
+    env = {}
+    hardware_el = root_el.find('{http://spec.org/test-environment}TestEnvironment/{http://spec.org/test-environment}SystemUnderTest/{http://spec.org/test-environment}Node/{http://spec.org/test-environment}Hardware')
+    if hardware_el:
+        env['vendor'] = hardware_el.find('{http://spec.org/test-environment}Vendor').text
+        env['model'] = hardware_el.find('{http://spec.org/test-environment}Model').text
+        env['cpu'] = hardware_el.find('{http://spec.org/test-environment}Cpu/{http://spec.org/test-environment}Name').text
+        env['dimms'] = hardware_el.find('{http://spec.org/test-environment}Memory/{http://spec.org/test-environment}Dimms/{http://spec.org/test-environment}Quantity').text
+        env['dimm_size_mb'] = hardware_el.find('{http://spec.org/test-environment}Memory/{http://spec.org/test-environment}Dimms/{http://spec.org/test-environment}DimmSizeMB').text
+        env['psu'] = hardware_el.find('{http://spec.org/test-environment}PowerSupplies/{http://spec.org/test-environment}PowerSupply/{http://spec.org/test-environment}RatingInWatts').text
+        env['ref'] = root_el.find('{http://spec.org/test-environment}TestEnvironment/{http://spec.org/test-environment}TestInformation/{http://spec.org/test-environment}InternalReference').text
+    else:
+        for node in root_el.iter():
+            print(node)
+    return env
 
 def merge(a, b, path=None):
     "merges b into a"
